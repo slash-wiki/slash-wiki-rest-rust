0ea5d9fe33b13f024046161a18915bfccd0371fb
Luna <75589876+they-them@users.noreply.github.com>
2020-12-13 10:47:44 -0500

    Add application

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 
+use std::time::SystemTime;
+use rocket::request::Form;
+use rocket_contrib::json::Json;
+use serde::{Serialize, Deserialize};
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![response]).launch();
 }
 
-#[post("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[post("/api", data = "<input>")]
+fn response(input: Form<Parameters>) -> Json<Message> {
+    let parameters = input.into_inner();
+
+    Json(Message {
+        response_type: "in_channel".to_string(),
+        text: format!("Hello {}, the date and time is {:#?}.", parameters.user_id, SystemTime::now()),
+    })
+}
+
+#[derive(Debug, FromForm)]
+pub struct Parameters {
+    pub token: String,
+    pub team_id: String,
+    pub team_domain: String,
+    pub channel_id: String,
+    pub channel_name: String,
+    pub user_id: String,
+    pub user_name: String,
+    pub command: String,
+    pub text: String,
+    pub response_url: String,
+    pub trigger_id: String,
+    pub api_app_id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Message {
+    pub response_type: String,
+    pub text: String,
 }
