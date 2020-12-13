12a941b5a51165b03d5b0186d168b6c041a75b03
Luna <75589876+they-them@users.noreply.github.com>
2020-12-13 10:21:08 -0500

    Add hello world app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
+}
+
+#[post("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
